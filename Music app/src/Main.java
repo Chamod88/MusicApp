f9110a74f4625226a4a29fa2041cfa9330f747c9
Chamod88 <124488371+Chamod88@users.noreply.github.com>
2023-04-29 17:51:18 +0530

    Add files via upload

---
+++ Music app/src/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Album album = new Album("Album1","Bicep");
+
+        album.addSong(title: "waterfall", duration )
+
+    }
+}
